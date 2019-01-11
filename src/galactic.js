7e1e4012def475d3132ecab580932d4e26c17957
Christopher Cahill <cthomascahill@gmail.com>
2019-01-11 11:09:06 -0800

    tests passing

--- src/galactic.js
+++ src/galactic.js
@@ -3,8 +3,12 @@ export function Galactic(year,month,day) {
   this.earthBirthday = new Date(year,month,day)
   this.earthYears = Math.floor(this.dateDifference()/365)
   this.mercuryYears = this.mercuryAge();
+  this.venusYears = this.venusAge();
+  this.marsYears = this.marsAge();
+  this.jupiterYears = this.jupiterAge();
 
 }
+
 Galactic.prototype.dateDifference = function() {
   let oneDay = 1000 * 60 * 60 * 24;
   let difference = this.dateNow - this.earthBirthday;
@@ -16,4 +20,22 @@ Galactic.prototype.mercuryAge = function() {
   const mercuryRatio = 0.24;
   let mercury = Math.floor(this.earthYears/mercuryRatio);
   return mercury;
+}
+
+Galactic.prototype.venusAge = function() {
+  const mercuryRatio = 0.62;
+  let mercury = Math.floor(this.earthYears/mercuryRatio);
+  return venus;
+}
+
+Galactic.prototype.marsAge = function() {
+  const mercuryRatio = 1.88;
+  let mercury = Math.floor(this.earthYears/mercuryRatio);
+  return mars;
+}
+
+Galactic.prototype.jupiterAge = function() {
+  const mercuryRatio = 11.86;
+  let mercury = Math.floor(this.earthYears/mercuryRatio);
+  return jupiter;
 }
