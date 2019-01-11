ec6d5251d02e69baec4bd69d97cf4d3a73bfcbc0
Christopher Cahill <cthomascahill@gmail.com>
2019-01-11 11:06:37 -0800

    added tests for venus mars and jupiter years

--- spec/galactic-spec.js
+++ spec/galactic-spec.js
@@ -5,12 +5,21 @@ let newUser;
 
   describe ('Galactic', function() {
     beforeEach(function() {
-      newUser = new Galactic(1989,10,17);
+    newUser = new Galactic(1989,10,17);
     })
   it('should return a normal earth age', function() {
-    expect(newUser.earthYears).toEqual(28);
+    expect(newUser.earthYears).toEqual(29);
   })
   it('should return a mercury age based upon earth age', function() {
-    expect(newUser.mercuryYears).toEqual(104);
+    expect(newUser.mercuryYears).toEqual(120);
+  })
+  it('should return a venus age based upon earth age', function() {
+    expect(newUser.venusYears).toEqual(46);
+  })
+  it('should return a mars age based upon earth age', function() {
+    expect(newUser.marsYears).toEqual(15);
+  })
+  it('should return a jupiter age based upon earth age', function() {
+    expect(newUser.jupiterYears).toEqual(2);
   })
 })
--- src/galactic.js
+++ src/galactic.js
@@ -5,17 +5,15 @@ export function Galactic(year,month,day) {
   this.mercuryYears = this.mercuryAge();
 
 }
-
-
-Galactic.prototype.mercuryAge = function() {
-  const mercuryRatio = 0.24;
-  let mercury = Math.floor(this.earthYears/mercuryRatio);
-  return mercury;
-}
-
 Galactic.prototype.dateDifference = function() {
   let oneDay = 1000 * 60 * 60 * 24;
   let difference = this.dateNow - this.earthBirthday;
   let daysDifference = difference/oneDay;
   return Math.floor(daysDifference);
+}
+
+Galactic.prototype.mercuryAge = function() {
+  const mercuryRatio = 0.24;
+  let mercury = Math.floor(this.earthYears/mercuryRatio);
+  return mercury;
 }
