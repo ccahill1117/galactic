7ff55c84af0f403634fed8006ac77e362c3370dc
Christopher Cahill <cthomascahill@gmail.com>
2019-01-11 16:04:14 -0800

    provides user ages in other planet years

--- src/main.js
+++ src/main.js
@@ -15,17 +15,24 @@ $(document).ready(function(){
   let userBirthday = ($("#userBirthday").val()).toString();
   let userBirthdayMonthParse =  parseInt(userBirthday.substring(5,7)) - 1
   let userGalaxy = new Galactic(
-    ((parseInt(userBirthday.substring(0,4))) - 1).toString(),
+    ((parseInt(userBirthday.substring(0,4)))).toString(),
     userBirthdayMonthParse,
     userBirthday.substring(8,10)
   );
+  console.log(userGalaxy);
 
   let userYear = ((userGalaxy.earthBirthday).getFullYear()).toString();
   let userMonth = monthName((userGalaxy.earthBirthday).getMonth()).toString();
   let userDate = (userGalaxy.earthBirthday.getDate()).toString();
   let userBday = `${userMonth} ${userDate}, ${userYear}`
-  $("#earthBirthday").text(userBday);
 
+  $("#earthBirthday").text(userBday);
+  $("#earthAge").text(userGalaxy.earthYears);
+  console.log(userGalaxy.earthYears)
+  $("#mercuryAge").text(userGalaxy.mercuryAge());
+  $("#venusAge").text(userGalaxy.venusAge());
+  $("#marsAge").text(userGalaxy.marsAge());
+  $("#jupiterAge").text(userGalaxy.jupiterAge());
 
 });
 
